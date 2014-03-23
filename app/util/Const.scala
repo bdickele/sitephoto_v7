d44f7e3d78c92ee6c14d29a0dc4394476f39ce73
Bertrand DICKELE <bdickele@gmail.com>
2014-03-23 22:43:35 +0100

    Minor adjustments

--- app/util/Const.scala
+++ app/util/Const.scala
@@ -6,8 +6,9 @@ package util
  */
 object Const {
 
-  val WebRoot = "http://www.dickele.com/photostock/"
+  //val WebRoot = "http://www.dickele.com/photostock/"
   //val WebRoot = "http://localhost:8080/photostock/"
+  val WebRoot = "http://dickele.cluster010.ovh.net/photostock/"
 
   val FolderWeb = "web/"
   val FolderThumbnail = "thumbnail/"
