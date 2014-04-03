18e4e2368820765f0812ce76f0fc5b40d2491198
Bertrand DICKELE <bdickele@gmail.com>
2014-04-03 20:39:31 +0200

    Localization and minor changes

--- app/Global.scala
+++ app/Global.scala
@@ -12,17 +12,22 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     val databaseMessage = app.configuration.getString("mongodb.uri") match {
-        case None => "Damn, could not find database configuration"
-        case Some(dbUri) =>
-          if (dbUri.contains("localhost")) "You're running on a LOCAL database"
-          else "Keep in mind you're running on a REMOTE database"
-      }
+      case None => "Damn, could not find database configuration"
+      case Some(dbUri) =>
+        if (dbUri.contains("localhost")) "You're running on a LOCAL database"
+        else "Keep in mind you're running on a REMOTE database"
+    }
     prettyLog(databaseMessage)
   }
 
   override def onHandlerNotFound(request: RequestHeader) =
     Future.successful(NotFound(
       views.html.notFound(request.path)
+    ))
+
+  override def onBadRequest(request: RequestHeader, error: String) =
+    Future.successful(BadRequest(
+      views.html.badRequest(error)
     ))
 
   override def onError(request: RequestHeader, ex: Throwable) =
